52d7c75f50534a8e9f04943c7be3f8a936a48835
matheusbrasilaguiar <matheusbrasilaguiar@gmail.com>
2025-01-11 22:00:41 -0300

    feat: :sparkles: Add GenericType entity class

---
+++ fleet-backend/src/main/java/com/fleet/domain/GenericType.java
@@ -0,0 +1,37 @@
+package com.fleet.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "generic_type")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenericType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(nullable = false, length = 50)
+    private String category;
+    
+}
