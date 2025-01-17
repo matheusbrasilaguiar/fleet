00331697a7e7658a2428c0ea2d72d1db8b17eab8
matheusbrasilaguiar <matheusbrasilaguiar@gmail.com>
2025-01-16 22:03:41 -0300

    feat: :white_check_mark: Add unit tests for TipoGenericoService

--- fleet-backend/src/main/java/com/fleet/dto/request/TipoGenericoRequest.java
+++ fleet-backend/src/main/java/com/fleet/dto/request/TipoGenericoRequest.java
@@ -2,11 +2,13 @@ package com.fleet.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TipoGenericoRequest {
 
     private String nome;
